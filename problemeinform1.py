9aa9e732312452fc57610acc84fbbe59eb53cb46
Valeria <70991795+VaLeRy013@users.noreply.github.com>
2020-09-22 19:41:52 +0300

    Add files via upload

---
+++ problemeinform1.py
@@ -0,0 +1,43 @@
+print("Problema 01")
+print("Struguri     100       kg")
+print("Mere         10      tone")
+print("Cartofi      250       kg")
+print("Varză        1000       q")
+
+print("Problema 02")
+a=3
+b=6
+print("a=3")
+print("b=6")
+print("a+b", a+b)
+print("a-b", a-b)
+print("a*b", a*b)
+print("a/b", a/b)
+print("Câtul din a/b=", a//b)
+print("Câtul din b/a=", b//a)
+print("Restul din a/b=", a%b)
+print("Restul din b/a=", b%a)
+print("a**b=", a**b)
+print("b**a=", b**a)
+
+print("Problema 03")
+L=int(input("Lungimea laturei cubului="))
+print("Volumul=", 4*L,"cm")
+print("Perimetrul", L**3,"cm3")
+print("Aria suprafețelor=",6*(L**2), "cm2")
+
+print("Problema 04")
+n=int(input("număr natural="))
+print(n, "metri în centimetri",n*100)
+print(n, "kilograme în miligrame", n*10000)
+print("În", n,"ani",n*52,"săptămâni",n*365, "zile", n*12,"luni")
+
+print("Problema 05")
+x=True
+y=False
+print(x and y)
+print(y and x)
+print(x or y)
+print(y or x)
+print(not x)
+print(not y)
